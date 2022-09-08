f2f0d2f74cdbede8017aa28c9f005a9fefb10dd5
YOGESHH1310 <yogesh20002@gmail.com>
2022-09-09 02:29:28 +0530

    finish

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-console.log('yogesh')
+// const audio = document.getElementById('audio');
+// const btn = document.getElementById('button');
+
+// function button() {
+//     const audio1 = audio.scroll;
+//     audio1.play();
+// }
+
+// button.addEventListener('click',button);
+
